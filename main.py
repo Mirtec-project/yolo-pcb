96d300656259a6b891a7e0dc49fa5acec686825f
cho <aaaapple123@naver.com>
2025-02-06 11:46:51 +0900

    0206_3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 dataset_path = "yolo_dataset/data.yaml"
 
 # 사용자 정의 모델 구조를 기반으로 YOLO 모델 생성
-model = YOLO('yolo-pcb_0206_1.yaml')
+model = YOLO('yolo-pcb_0206_3.yaml')
 
 # 모델 학습 진행
 results = model.train(
